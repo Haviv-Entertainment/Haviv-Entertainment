dc3e7ee6e78ad08c6ec1b050f12aae39dcf173ae
Sara Eckhaus <saraeckhaus@Saras-MacBook-Pro.local>
2018-05-15 16:24:11 -0400

    added contact form jsx, added formerrors.js component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,15 +1,285 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { FormErrors } from './FormErrors';
 import Nav from './Nav';
 import Logo from './Logo';
 // import '../styling/home.css';
 
-const Contact = () => {
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      number: '',
+      message: '',
+      timestamp: Date.now(),
+      formHidden: false,
+      formResponse: false,
+      formErrors: {name: '', email: '', number: '', message: ''},
+      nameValid: false,
+      emailValid: false,
+      phoneNumberValid: false,
+      messageValid: false,
+      formValid: false
+    };
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  onChange(event) {
+    const name = event.target.name;
+  const value = event.target.value;
+    this.setState({[name]: value},
+              () => { this.validateField(name, value) });
+}
+
+validateField(fieldName, value) {
+let fieldValidationErrors = this.state.formErrors;
+let nameValid = this.state.nameValid;
+let emailValid = this.state.emailValid;
+let phoneNumberValid = this.state.phoneNumberValid;
+let messageValid = this.state.messageValid;
+
+switch(fieldName) {
+case 'name':
+    nameValid = value.match(/^[a-z ,.'-]+$/i) && value.length >= 2;
+    fieldValidationErrors.name = nameValid ? '': ' is invalid';
+    break;
+case 'email':
+    emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+    break;
+case 'number':
+    phoneNumberValid = value.length >= 10;
+    fieldValidationErrors.number = phoneNumberValid ? '': ' is too short';
+    break;
+case 'message':
+    messageValid = value.match(/^[a-zA-Z0-9 ,.!'-]+$/i) && value.length >= 2;
+    fieldValidationErrors.message = messageValid ? '': ' is invalid';
+    break;
+  default:
+    break;
+
+}
+
+  // console.log("errors", fieldValidationErrors)
+
+  this.setState({formErrors: fieldValidationErrors,
+                nameValid: nameValid,
+                emailValid: emailValid,
+                phoneNumberValid: phoneNumberValid,
+                messageValid: messageValid
+              }, this.validateForm);
+  }
+
+  validateForm() {
+  const {
+    nameValid,
+    emailValid,
+    phoneNumberValid,
+    messageValid
+  } = this.state;
+
+  this.setState({
+    formValid: nameValid && emailValid && phoneNumberValid && messageValid
+  });
+  }
+
+  errorClass(error) {
+  return(error.length === 0 ? '' : 'has-error');
+  }
+
+
+  onSubmit(event) {
+      event.preventDefault();
+
+  // get our form data out of state
+  const { name, email, number, message, timestamp } = this.state;
+
+  let myDate = new Date(timestamp);
+  let formatedTime = myDate.toJSON();
+
+  fire.database().ref().push({
+  name: name,
+  email: email,
+  number: number,
+  message: message,
+  timestamp: formatedTime
+  }).then((response) => {
+  // console.log(response)
+  })
+  .catch(function (error) {
+  console.log(error)
+  });
+
+  this.formHider()
+  this.onFormSubmit()
+
+
+  }
+
+  formHider() {
+  this.setState(currentState => ({
+  formHidden: !currentState.formHidden
+  }), () => console.log(`Toggling visibility of Header!: ${this.state.formHidden}`))
+  }
+
+  onFormSubmit() {
+  this.setState(currentState => ({
+  formResponse: !currentState.formResponse
+  }), () => console.log(`Toggling visibility of Header!: ${this.state.formResponse}`))
+  }
+
+
+render() {
+
+  const {
+    name,
+    email,
+    number,
+    message,
+    formResponse,
+    formHidden,
+    formErrors,
+    formValid
+  } = this.state;
+
+
+if(formErrors.name.toString() === 'name') {
+  console.log('helloooooo')
+}
+
     return (
-      <div>
+      <div id="contactForm">
         <Logo />
         <Nav />
+        <form
+          method="POST"
+          action="send"
+          id="contactForm"
+          className="wow bounceInLeft formContainerStyle"
+          onSubmit={this.onSubmit}
+        >
+          <FormErrors
+            formErrors={formErrors}
+            className="formErrorContainer"
+          />
+        <div className=
+            {`
+              form-group ${this.errorClass(formErrors.name)}
+            `}
+          >
+            <div
+              className="labelContainer"
+            >
+              <label
+                className="labelStyle"
+                htmlFor="name"
+              >
+                Name
+              </label>
+            </div>
+            <input
+              className="inputStyle"
+              type="text"
+              placeholder="John Appleseed"
+              id="name"
+              name="name"
+              value={name}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className=
+            {`
+              form-group ${this.errorClass(formErrors.email)}
+            `}
+          >
+            <div
+              className="labelContainer"
+            >
+              <label
+                className="labelStyle"
+                htmlFor="email"
+              >
+                Email
+              </label>
+            </div>
+            <input
+              className="inputStyle"
+              type="email"
+              placeholder="john123@john.com"
+              id="email"
+              name="email"
+              value={email}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className=
+            {`
+              form-group ${this.errorClass(formErrors.number)}
+            `}
+          >
+            <div
+              className="labelContainer"
+            >
+              <label
+                className="labelStyle"
+                htmlFor="number"
+              >
+                Phone Number
+              </label>
+            </div>
+            <input
+              className="inputStyle"
+              type="tel"
+              placeholder="1234567890"
+              id="phoneNumber"
+              name="number"
+              value={number}
+              onChange={this.onChange}
+            />
+          </div>
+          <div className=
+            {`
+              form-group ${this.errorClass(formErrors.message)}
+            `}
+          >
+            <div
+              className="labelContainer"
+            >
+              <label
+                className="labelStyle"
+                htmlFor="message"
+              >
+                Message
+              </label>
+            </div>
+            <textarea
+              className="inputStyle textareaStyle"
+              rows="12"
+              cols="72"
+              id="message"
+              name="message"
+              value={message}
+              onChange={this.onChange}
+            />
+          </div>
+          <button
+            className="buttonStyle"
+            type="submit"
+            disabled={!formValid}
+          >
+            Submit
+          </button>
+        </form>
+        <footer
+          className="footerStyle"
+        >
+          Copyright © 2018 Michael Haviv - All Rights Reserved.
+        </footer>
       </div>
     );
-  };
-  
-  export default Contact;
+  }
+}
+export default Contact;
---
+++ src/components/FormErrors.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "../css/formerrors.css";
+
+
+export const FormErrors = ({formErrors}) => {
+  <div className='formErrors'>
+    {Object.keys(formErrors).map((fieldName, i) => {
+      if(formErrors[fieldName].length > 0){
+        return (
+            <p
+              key={i}
+              className="formErrorStyle"
+          >
+            {fieldName} {formErrors[fieldName]}
+          </p>
+        )
+      } else {
+        return '';
+      }
+    })}
+  </div>
+}
